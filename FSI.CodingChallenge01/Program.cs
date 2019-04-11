f497c9a6baa7c464dcd4a743eb7b98302cf61ace
BigMo <big@mo.com>
2019-04-11 09:55:04 +0200

    Projektdateien hinzufügen.

---
+++ FSI.CodingChallenge01/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("{0} von deinen {1} Steinen sind Edelsteine!", "aA".Sum(e => "aAAbbbb".Count(s => s == e)), "aAAbbbb".Length);
+    }
+}
